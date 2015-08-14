f85317c592d7724c481100bd1862afb42dfca122
Bailey <baileypiggott@gmail.com>
2015-08-14 13:08:09 -0400

    add text to sidebar that describes app

--- ui.R
+++ ui.R
@@ -19,7 +19,18 @@ shinyUI(fluidPage(
                 )#end options
     ),#end selectInput
     
-    downloadButton("downloadPDF", "Download Plot")
+    downloadButton("downloadPDF", "Download Plot"),
+    
+    #text description of app ------------------------
+    
+    br(),br(), 
+    h4("About This App"),
+    p("This app displays the CAT (Critical Thinking Assessment Test) scores of Queen's students from 4
+      departments. This test has a maximum score of 36."),
+    br(),
+    p("Learn more about the HEQCO Learning Outcomes Assessment Project", 
+      a("here.", href = "http://www.queensu.ca/qloa/"))
+    # end text description    
     
     
   ),#end sidebar panel
